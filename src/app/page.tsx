1767705e0c9ad7b6cfb9e29d070acbe3833a3f93
Giaco <jacopo.albrizio@gmail.com>
2023-06-26 11:08:09 +0200

    feat: cobe — rotate to location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Cobe from "@/components/cobe";
 
 export default function Home() {
-  return <p>ao</p>;
+  return <Cobe />;
 }
