270a52ccc314c78a1994b218fd36efbf1aa02fb1
Giaco <jacopo.albrizio@gmail.com>
2023-06-30 12:14:45 +0200

    remove all redis/rate-limit related code

--- src/lib/rate-limiter.ts
+++
@@ -1,8 +0,0 @@
-import { redis } from './redis'
-import { Ratelimit } from "@upstash/ratelimit";
-
-export const rateLimiter = new Ratelimit({
-    redis,
-    limiter: Ratelimit.slidingWindow(3, '15 s'),
-    prefix: '@upstash/ratelimit',
-})
