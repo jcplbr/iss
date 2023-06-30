a304f5b6e13c42307addf49b1394b6ac7e17551b
Giaco <jacopo.albrizio@gmail.com>
2023-06-30 11:46:24 +0200

    another one

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
-import { rateLimiter } from "./lib/rate-limiter";
+// import { NextRequest, NextResponse } from "next/server";
+// import { rateLimiter } from "./lib/rate-limiter";
 
-export async function middleware(req: NextRequest) {
-    const ip = req.ip ?? '127.0.0.1'
+// export async function middleware(req: NextRequest) {
+//     const ip = req.ip ?? '127.0.0.1'
 
-    try {
-        const { success } = await rateLimiter.limit(ip)
+//     try {
+//         const { success } = await rateLimiter.limit(ip)
 
-        if (!success) return new Response('Too many requests.', { status: 429 })
+//         if (!success) return new Response('Too many requests.', { status: 429 })
 
-        return NextResponse.next()
-    } catch (error) {
-        console.log('Error:', error)
+//         return NextResponse.next()
+//     } catch (error) {
+//         console.log('Error:', error)
 
-        return new NextResponse(
-            'Sorry, something went wrong processing your message. Please try again later.'
-        )
-    }
-}
+//         return new NextResponse(
+//             'Sorry, something went wrong processing your message. Please try again later.'
+//         )
+//     }
+// }
 
-export const config = {
-    matcher: '/api/issData/:path*',
-}
+// export const config = {
+//     matcher: '/api/issData/:path*',
+// }
